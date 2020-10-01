981b2d36a319c98675b18fc5844b896c9c61c7bd
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-10-01 13:32:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hypotenuse_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double legOne;
+            Console.WriteLine("Please enter the length of the first triangle leg.");
+            Console.ReadLine();
+
+            double legTwo;
+            Console.WriteLine("Please enter the length of the second triangle leg.");
+            Console.ReadLine();
+
+            double hypo = legOne
+            Console.WriteLine($"The length of your hypotenuse is {}.")
+
+            Console.ReadLine();
+        }
+    }
+}
